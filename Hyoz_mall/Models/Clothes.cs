49224d14064216fce615529c033953806cad33cd
ingyeoking13 <ingyeoking13@gmail.com>
2019-05-15 22:07:13 +0900

    dd

---
+++ Hyoz_mall/Models/Clothes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyoz_mall.Models
+{
+    public class Clothes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="옷 이름")]
+        public string Name { get; set; }
+        [Display(Name ="디자이너")]
+        public string Designer { get; set; }
+        [Display(Name ="가격")]
+        public int KRW { get; set; }
+    }
+}
